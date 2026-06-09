def018881410eb382e6ff93b5b556a8ea0a27008
EstebanMartin <6530285+EstebanMartin@users.noreply.github.com>
2026-06-08 23:15:35 -0600

    fish: set CDPATH to ~/.config for quick navigation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@
 # =============================================================================
 set -l os (uname)
 
+set -gx CDPATH $HOME/.config
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
